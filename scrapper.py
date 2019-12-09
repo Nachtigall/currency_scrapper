012e671f68823abd65812cc5a823a97e31e3d046
Oles Mikula <torba.o@gmail.com>
2019-12-09 16:53:59 +0200

    Small refactoring

--- scrapper.py
+++ scrapper.py
@@ -8,7 +8,8 @@ import requests
 import pandas as pd
 from pandas import DataFrame
 
-def get_download_url(url):
+
+def get_download_url(url: str):
     res = requests.get(f'{url}/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html')
     soup = BeautifulSoup(res.text, 'html.parser')
 
@@ -20,7 +21,8 @@ def get_download_url(url):
 
     return url_with_hist_data
 
-def download_zip_csv(url, url_to_download, path):
+
+def download_zip_csv(url: str, url_to_download: str, path: str) -> str:
     res = requests.get(f'{url}{url_to_download}')
     files = zipfile.ZipFile(io.BytesIO(res.content))
     files.extractall(path)
@@ -36,32 +38,33 @@ def download_zip_csv(url, url_to_download, path):
     return filenames[0]
 
 
-def _filter_helper(file):
+def _filter_helper(file: str, columns_to_filter: list) -> list:
     data = pd.read_csv(file)
 
     filtered_columns = DataFrame(data, columns=columns_to_filter)
 
-    date_cond = pd.to_datetime(data['Date']) > pd.Timestamp(2018,12,31)
+    date_cond = pd.to_datetime(data['Date']) > pd.Timestamp(2018, 12, 31)
     filtered_data = filtered_columns.loc[date_cond]
 
     return filtered_data
 
-def csv_by_dates(path, csv_file, columns_to_filter):
 
-    data = _filter_helper(f'{path}/{csv_file}')
+def csv_by_dates(path: str, csv_file: str, columns_to_filter: list) -> str:
+    data = _filter_helper(f'{path}/{csv_file}', columns_to_filter)
     clean_data = pd.DataFrame(columns=['Date', 'DKK', 'CAD'])
 
-    for index,row in data.iterrows():
-        clean_data = clean_data.append({'Date': row['Date'].replace('-',''),
-        'DKK': row['DKK'], 'CAD': row['CAD']}, ignore_index=True)
+    for index, row in data.iterrows():
+        clean_data = clean_data.append({'Date': row['Date'].replace('-', ''),
+                                        'DKK': row['DKK'], 'CAD': row['CAD']}, ignore_index=True)
 
     filename = f'{path}/rates_by_dates.csv'
     clean_data.to_csv(filename, index=False)
 
     return filename
 
-def csv_by_monthes(path, csv_file, columns_to_filter):
-    dirty_data = _filter_helper(f'{path}/{csv_file}')
+
+def csv_by_months(path: str, csv_file: str, columns_to_filter: list) -> str:
+    dirty_data = _filter_helper(f'{path}/{csv_file}', columns_to_filter)
     dirty_data['Date'] = pd.to_datetime(dirty_data['Date'])
 
     clean_data = dirty_data.set_index('Date').resample('M')['DKK', 'CAD'].mean()[::-1]
@@ -71,27 +74,27 @@ def csv_by_monthes(path, csv_file, columns_to_filter):
     clean_data['Date'] = clean_data.Date.dt.strftime('%Y%m')
     clean_data = clean_data.rename(columns={'Date': 'Month'})
 
-    filename = f'{path}/rates_by_monthes.csv'
+    filename = f'{path}/rates_by_months.csv'
     clean_data.to_csv(filename, index=False)
 
     return filename
 
+
 if __name__ == '__main__':
     root_url = 'https://www.ecb.europa.eu'
     current_folder = os.path.dirname(os.path.realpath(__file__))
-    columns_to_filter = ['Date', 'DKK', 'CAD']
+    columns_filter = ['Date', 'DKK', 'CAD']
 
-    url_to_download = get_download_url(root_url)
-    raw_file = download_zip_csv(root_url, url_to_download, current_folder)
-    csv_by_dates_name = csv_by_dates(current_folder, raw_file, columns_to_filter)
-    csv_by_monthes_name = csv_by_monthes(current_folder, raw_file, columns_to_filter)
-
+    download_url = get_download_url(root_url)
+    raw_file = download_zip_csv(root_url, download_url, current_folder)
+    csv_by_dates_name = csv_by_dates(current_folder, raw_file, columns_filter)
+    csv_by_months_name = csv_by_months(current_folder, raw_file, columns_filter)
 
     result = f""" ============= Results =============
     Current folder: {current_folder}
     Root url: {root_url}
     Raw file: {current_folder}/{raw_file}
     Rates by Date CSV: {current_folder}/{csv_by_dates_name}
-    Rates by Month CSV:{current_folder}/{csv_by_monthes_name}
+    Rates by Month CSV:{current_folder}/{csv_by_months_name}
     """
     sys.stdout.write(result)
